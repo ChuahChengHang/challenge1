d367f9c0fdf973678d2684441356db98cc82a86f
NoUser <chuah_cheng_hang@s2024.ssts.edu.sg>
2025-05-31 14:50:34 +0800

    Initial Commit

---
+++ Challenge 1/Challenge_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_1App.swift
+//  Challenge 1
+//
+//  Created by Chuah Cheng Hang on 31/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge 1
+//
+//  Created by Chuah Cheng Hang on 31/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
